11717caacf44d4fb35f041ec96c31f5a61866dd4
Christofferos <42782387+Christofferos@users.noreply.github.com>
2019-04-16 21:45:21 +0200

    Significant changes made to the snake game. Proper collision detection and difficulty increase by adding walls onto the map. Fancy typewriter implemented at loss screen aswell.

--- games/snake/main.js
+++ games/snake/main.js
@@ -1,10 +1,33 @@
-
 $(document).ready(function(){
+
+	class Rectangle {
+		constructor(x1, x2, y1, y2) {
+			this.x1 = x1;
+			this.x2 = x2;
+			this.y1 = y1;
+			this.y2 = y2;
+		}
+		collisionDetection(checkx, checky) {
+			if(checkx >= this.x1 && checkx <= this.x2 && checky >= this.y1 && checky <= this.y2)
+				return true;
+			else
+				return false;
+		}
+	}
 
 	let gameArea = $("#game-area");
 	let maxGameArea = 20;
 	var hasPressed = false;
+	
+	var easyDifficulty = 1;
+	var mediumDifficulty = 25;
+	var hardDifficulty = 50;
+	var maxedDifficulty = 75;
 
+	var notAllowedSpawnsX = [];
+	var notAllowedSpawnsY = [];
+
+	var rectangleList = [];
 
 	let playerLength = 4;
 	let playerPos = {
@@ -19,6 +42,7 @@ $(document).ready(function(){
 	};
 	let currentPlayerDir = playerDir["right"];
 
+	// First point 
 	let pointPosX = Math.floor(Math.random() * 19) + 0;
 	let pointPosY = Math.floor(Math.random() * 19) + 0;
 	let pointPos = {
@@ -49,7 +73,7 @@ $(document).ready(function(){
 
 
 	function drawPoint() {
-		let setPointPos = $(".tr" + pointPos["posX"] + "td" + pointPos["posY"]);
+		let setPointPos = $(".tr" + pointPos["posY"] + "td" + pointPos["posX"]);
 
 		setPointPos.addClass("draw-point");
 	}
@@ -57,25 +81,77 @@ $(document).ready(function(){
 	drawPoint();
 
 	function playerEatsPoint() {
-		let setPointPos = $(".tr" + pointPos["posX"] + "td" + pointPos["posY"]);
+		let setPointPos = $(".tr" + pointPos["posY"] + "td" + pointPos["posX"]);
 		
 		setPointPos.removeClass("draw-point");
 
-		pointPosX = Math.floor(Math.random() * 19) + 0;
-		pointPosY = Math.floor(Math.random() * 19) + 0;
+		rectangleList.push(new Rectangle(5, 9, 5, 6), new Rectangle(15, 16, 10, 14));
+		//rectangleList.push(new Rectangle(2, 15, 5, 6), new Rectangle(1, 19, 10, 19));
 
+		let newSpawn = false;
+		do {
+			newSpawn = false;
+			pointPosX = Math.floor(Math.random() * 19) + 0;
+			pointPosY = Math.floor(Math.random() * 19) + 0;
+			for(let i = 0; i < rectangleList.length; i++) {
+				if(rectangleList[i].collisionDetection(pointPosX, pointPosY)) {
+					newSpawn = true;
+				}
+			}
+		}
+		while (newSpawn)
+		
 		pointPos = {
 			"posX": pointPosX,
 			"posY": pointPosY
 		};
 
-		setPointPos = $(".tr" + pointPos["posX"] + "td" + pointPos["posY"]);
+		setPointPos = $(".tr" + pointPos["posY"] + "td" + pointPos["posX"]);
 
 		setPointPos.addClass("draw-point");
 
 		gameScore += 1;
 
 		playerLength += 1;
+
+
+		if (gameScore == easyDifficulty) {	//10
+			playerPos = {
+				"tr": 18,
+				"td": 0
+			};
+			currentPlayerDir = playerDir["right"];
+			document.getElementById("game-status").innerHTML = "";
+			document.getElementById("game-status").innerHTML = "<span class='color-class2'>" + "Easy...";			
+
+			for(let i = 0; i < rectangleList.length; i++) {
+				for(let j = rectangleList[i].x1; j <= rectangleList[i].x2; j++) {
+					for(let k = rectangleList[i].y1; k <= rectangleList[i].y2; k++) {
+						document.getElementById("game-area").rows[k].cells[j].style.backgroundColor = "#ff6600";		
+					}		
+				}
+			}
+		}
+
+		if (gameScore == mediumDifficulty) {	//25
+			playerPos = {
+				"tr": 18,
+				"td": 0
+			};
+			currentPlayerDir = playerDir["right"];
+			document.getElementById("game-status").innerHTML = "";
+			document.getElementById("game-status").innerHTML = "<span class='color-class3'>" + "Not bad. <br>Difficulty: Medium";
+		}
+
+		if (gameScore == hardDifficulty) {	//50
+			document.getElementById("game-status").innerHTML = "";
+			document.getElementById("game-status").innerHTML = "<span class='color-class4'>" + "Impressive! <br>Difficulty: Hard";
+		}
+		
+		if (gameScore == maxedDifficulty) { //75
+			document.getElementById("game-status").innerHTML = "";
+			document.getElementById("game-status").innerHTML = "<span class='color-class5'>" + "Legendary!! <br>Difficulty: Maxed Out";
+		}
 	}
 
 	
@@ -137,8 +213,27 @@ $(document).ready(function(){
 
 	scoreHandler();
 
+	var hasBeenPrinted = false;
+
 	function deathHandler() {
-		document.getElementById("game-status").innerHTML = "You Loose!";
+		var i = 0; 
+		var txt = "GAME OVER! Refresh the website to play again.";
+		if (hasBeenPrinted == false) {
+			document.getElementById("game-status").innerHTML = "";
+			setInterval(typeWriter, 100);
+			typeWriter();
+		}
+
+		function typeWriter() {
+			if (i < txt.length) {
+				document.getElementById("game-status").innerHTML += "<span class='color-class'>" + txt.charAt(i);
+				if (i == 9) {
+					document.getElementById("game-status").innerHTML += "<br>";
+				}
+				i++;
+			}
+		}
+		hasBeenPrinted = true;
 	}
 
 	setInterval(function(){
@@ -163,6 +258,14 @@ $(document).ready(function(){
 				break;
 		}
 
+		//Check worm collision against spawned obstacles
+		for(let i = 0; i < rectangleList.length; i++) {
+			if(rectangleList[i].collisionDetection(playerPos["td"], playerPos["tr"])) {
+				deathHandler();
+				playerIsDead = true;
+			}
+		}
+
 		// Death by wall touch
 		if (playerPos["tr"] == 20 || playerPos["td"] == 20 || playerPos["tr"] == -1 || playerPos["td"] == -1) {
 			deathHandler();
@@ -174,7 +277,7 @@ $(document).ready(function(){
 			playerIsDead = true;
 		}
 		//Gain point
-		else if (playerPos["tr"] == pointPos["posX"] && playerPos["td"] == pointPos["posY"] && playerIsDead == false) {
+		else if (playerPos["tr"] == pointPos["posY"] && playerPos["td"] == pointPos["posX"] && playerIsDead == false) {
 			playerEatsPoint();
 			// Let the player do input again.
 			hasPressed = false;
