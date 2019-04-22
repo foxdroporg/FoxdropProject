e79585e71ce146d112b51ded99eaecc279453be4
Christofferos <42782387+Christofferos@users.noreply.github.com>
2019-04-22 22:02:37 +0200

    Video edited

--- contact.php
+++ contact.php
@@ -1,21 +1,20 @@
 <?php
-	include_once 'header.php';
+    include_once 'header.php';
 ?>
+    
 
 
-   
    <head>
     <meta charset="UTF-8">
      <title>Moutain View</title>
-     <style>
-     	body {
-     		background-color: #7C1D25;
-     	}
-     </style>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/p5.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/addons/p5.dom.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/addons/p5.sound.min.js"></script>
-    <script type="text/javascript" src="mountains.js"></script>
+
+    <div style="width:1000px color:white">
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/p5.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/addons/p5.dom.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/addons/p5.sound.min.js"></script>
+        <script type="text/javascript" src="mountains.js"></script>
+    </div>
+    
   </head>
   
 
--- mountains.js
+++ mountains.js
@@ -1,14 +1,27 @@
 var cols, rows;
 var scl = 20;
-var w = 1200;
-var h = 1400;
+var w = 1000;
+var h = 500;
 
 var flying = 0;
 
 var terrain = [];
 
+var cnv;
+
+function centerCanvas() {
+  var cnvPosX = (windowWidth - width) / 2;
+  var cnvPosY = (windowHeight - height) / 2;
+  cnv.position(cnvPosX, cnvPosY);
+}
+
+function windowResized() {
+  centerCanvas();
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight-65, WEBGL);
+  cnv = createCanvas(600, 400, WEBGL); // For fullscreen make (windowWidth, windowHeight-65)
+ centerCanvas();
   cols = w / scl;
   rows = h/ scl;
 
@@ -22,19 +35,19 @@ function setup() {
 
 function draw() {
 
-  flying -= 0.1;
+  flying -= 0.025;
   var yoff = flying;
   for (var y = 0; y < rows; y++) {
     var xoff = 0;
     for (var x = 0; x < cols; x++) {
-      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
+      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -80, 80);
       xoff += 0.2;
     }
     yoff += 0.2;
   }
 
 
-  background(0);
+  background(250);
   translate(0, 50);
   rotateX(5*PI/12);
   fill(200,200,200, 50);
