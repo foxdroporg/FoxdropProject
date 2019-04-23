a23e821a8fe314f810a8e3d0dc45797a8161acc9
Christofferos <42782387+Christofferos@users.noreply.github.com>
2019-04-23 18:39:05 +0200

    Tried to fix styling for portfolio

--- mountains.js
+++ mountains.js
@@ -1,17 +1,17 @@
-var cols, rows;
+var columns, rows;
 var scl = 20;
 var w = 1000;
 var h = 500;
 
-var flying = 0;
+var birdPerspective = 0;
 
-var terrain = [];
+var mountainTerrain = [];
 
 var cnv;
 
 function centerCanvas() {
   var cnvPosX = (windowWidth - width) / 2;
-  var cnvPosY = (windowHeight - height) / 2;
+  var cnvPosY = (windowHeight - height) / 4;
   cnv.position(cnvPosX, cnvPosY);
 }
 
@@ -22,41 +22,40 @@ function windowResized() {
 function setup() {
   cnv = createCanvas(600, 400, WEBGL); // For fullscreen make (windowWidth, windowHeight-65)
  centerCanvas();
-  cols = w / scl;
+  columns = w / scl;
   rows = h/ scl;
 
-  for (var x = 0; x < cols; x++) {
-    terrain[x] = [];
+  for (var x = 0; x < columns; x++) {
+    mountainTerrain[x] = [];
     for (var y = 0; y < rows; y++) {
-      terrain[x][y] = 0; //specify a default value for now
+      mountainTerrain[x][y] = 0; //specify a default value for now
     }
   }
 }
 
 function draw() {
-
-  flying -= 0.025;
-  var yoff = flying;
+  birdPerspective -= 0.05;
+  var yoffset = birdPerspective;
   for (var y = 0; y < rows; y++) {
-    var xoff = 0;
-    for (var x = 0; x < cols; x++) {
-      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -80, 80);
-      xoff += 0.2;
+    var xoffset = 0;
+    for (var x = 0; x < columns; x++) {
+      mountainTerrain[x][y] = map(noise(xoffset, yoffset), 0, 1, -80, 80);
+      xoffset += 0.2;
     }
-    yoff += 0.2;
+    yoffset += 0.2;
   }
 
 
-  background(250);
+  background(color(47, 9, 50));
   translate(0, 50);
   rotateX(5*PI/12);
   fill(200,200,200, 50);
   translate(-w/2, -h/2);
   for (var y = 0; y < rows-1; y++) {
     beginShape(TRIANGLE_STRIP);
-    for (var x = 0; x < cols; x++) {
-      vertex(x*scl, y*scl, terrain[x][y]);
-      vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
+    for (var x = 0; x < columns; x++) {
+      vertex(x*scl, y*scl, mountainTerrain[x][y]);
+      vertex(x*scl, (y+1)*scl, mountainTerrain[x][y+1]);
     }
     endShape();
   }
