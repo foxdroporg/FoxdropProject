f36050b9a0beb61dd340b19c363062ab0a6cc57f
Christofferos <42782387+Christofferos@users.noreply.github.com>
2019-04-24 23:38:33 +0200

    I centered text so it looks cleaner

--- portfolio.php
+++ portfolio.php
@@ -2,40 +2,40 @@
 	include_once 'header.php';
 ?>
 
-<section class="main-wrapper">
-	
-	<div class="main-wrapperportfolio">
-			<h2 style="color:#FFFFFF">Portfolio</h2>
-			<form class="portfolio-collection" action="games/snake/index.html" method="POST">
-				<input type="text" placeholder="Search for game">
-			</form>
-	</div>
+<section class="main-container">
+	<section class="main-wrapper">
+		
+		<div class="main-wrapperportfolio">
+				<h2 style="color:#FFFFFF">Portfolio</h2>
+				<form class="portfolio-collection" action="games/snake/index.html" method="POST">
+					<input type="text" placeholder="Search for game">
+				</form>
+		</div>
 
-	<div class="row">
-		<div class="column">
-		    <a href="games/pong/index.html" style="border:5px solid #ccc;">
-			<img src="images/pong.png" alt="4thGame" style="width:95%; height:95%; border: #000000 6px outset">
-			
-			</a>
+		<div class="row">
+			<div class="column">
+			    <a href="games/pong/index.html" style="border:5px solid #ccc;">
+				<img src="images/pong.png" alt="4thGame" style="width:95%; height:95%; border: #000000 6px outset">
+				
+				</a>
+			</div>
+			<div class="column">
+				<a href="games/tictactoe/difficulty.html" style="border:5px solid #ccc;">
+			    <img src="images/TicTacToe.png" alt="2ndGame" style="width:80%; height:85%; border: #000000 6px outset">
+				</a>
+			</div>
+			<div class="column">
+				<a href="games/snake/index.html" style="border:5px solid #ccc;">
+			    <img src="images/snake.jpg" alt="3rdGame" style="width:75%; height:75%; border: #000000 6px outset">
+				</a>
+			</div>
+			<div class="column">
+				<a href="games/maze/index.html" style="border:5px solid #ccc;">
+			    <img src="images/maze.jpg" alt="4thGame" style="width:70%; height:50%; border: #000000 6px outset">
+				</a>
+			</div>
 		</div>
-		<div class="column">
-			<a href="games/tictactoe/difficulty.html" style="border:5px solid #ccc;">
-		    <img src="images/TicTacToe.png" alt="2ndGame" style="width:80%; height:85%; border: #000000 6px outset">
-			</a>
-		</div>
-		<div class="column">
-			<a href="games/snake/index.html" style="border:5px solid #ccc;">
-		    <img src="images/snake.jpg" alt="3rdGame" style="width:75%; height:75%; border: #000000 6px outset">
-			</a>
-		</div>
-		<div class="column">
-			<a href="games/maze/index.html" style="border:5px solid #ccc;">
-		    <img src="images/maze.jpg" alt="4thGame" style="width:70%; height:50%; border: #000000 6px outset">
-			</a>
-		</div>
-	</div>
-
-
+	</section>
 </section>
 
 
--- reset-password.php
+++ reset-password.php
@@ -6,23 +6,24 @@
 
 <main>
 	<body>
-		<div class="main-wrapper">
-			<section class="main-container">
-					<h2 style="color:#FFFFFF">Reset your password</h2>
-					<p align="center" style="color:#FFFFFF">An e-mail will be send to you with instructions on how to reset your password.</p>
-					<form action="includes/reset-request.inc.php" method="POST">
-						<input type="text" name="email" placeholder="Enter your e-mail address...">
-						<button type="submit" name="reset-request-submit">Recieve new password by e-mail</button>
-					</form>
-					<?php
-						if(isset($_GET["reset"])) {
-							if ($_GET["reset"] == "success") {
-								echo '<p class="signupsuccess">Check your e-mail!</p>';
+		<section class="main-container">
+			<div class="main-wrapper">
+				
+						<h2 style="color:#FFFFFF">Reset your password</h2>
+						<p align="center" style="color:#FFFFFF">An e-mail will be send to you with instructions on how to reset your password.</p>
+						<form class="signup-form" action="includes/reset-request.inc.php" method="POST">
+							<input type="text" name="email" placeholder="Enter your e-mail address...">
+							<button type="submit" name="reset-request-submit">Recieve new password by e-mail</button>
+						</form>
+						<?php
+							if(isset($_GET["reset"])) {
+								if ($_GET["reset"] == "success") {
+									echo '<p class="signupsuccess">Check your e-mail!</p>';
+								}
 							}
-						}
-					?>
-			</section>
-		</div>
+						?>
+			</div>
+		</section>
 	</body>
 </main>
 
