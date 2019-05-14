c140b8c8fc534018ff6f03090b9a415da891ac7e
Christofferos <42782387+Christofferos@users.noreply.github.com>
2019-05-14 20:42:26 +0200

    Trying to fix parallex scroll

--- startpage.php
+++ startpage.php
@@ -1,15 +1,28 @@
 <?php
 	include_once 'header.php';
 ?>
+<!DOCTYPE html>
+<html>
 
-<section class="main-container">
-	<div class="main-wrapper">
-			<h2 style="color:#FFFFFF">Latest News</h2>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Start Page</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
 
-			
+	<section class="main-container">
+		<div class="main-wrapper">
+				<h2 style="color:#FFFFFF">Latest News</h2>
+		</div>
+	</section>
 
-	</div>
-</section>
+
+
+</html>
+
+
 
 
 
