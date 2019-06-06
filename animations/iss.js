251acbef09fba2348e6d52cc87dae1162ce3e3ae
Christofferos <42782387+Christofferos@users.noreply.github.com>
2019-06-06 17:07:31 +0200

    International Space Station real-time animation finished

---
+++ animations/iss.js
@@ -0,0 +1,37 @@
+var song;
+var buttonToggle;
+
+function preload() {
+  song = loadSound("soundeffects/satellit.mp3", loaded);
+}
+
+function setup() {
+	buttonToggle = createButton("Satellit song");
+	buttonToggle.mousePressed(togglePlaying);
+	centerCanvas();
+  	song.setVolume(0.8);
+}
+
+function centerCanvas() {
+	var cnvPosX = (windowWidth - width) / 2;
+  	var cnvPosY = (windowHeight - height) / 4;
+  	buttonToggle.position(cnvPosX/2 + 25, cnvPosY/1.5);
+}
+
+function windowResized() {
+  centerCanvas();
+}
+
+function togglePlaying() {
+	if (!song.isPlaying()) {
+		song.loop();
+		buttonToggle.html("Turn off sattelit song");
+	} else {
+		song.pause();
+		buttonToggle.html("Satellit song");
+	}
+}
+
+function loaded() {
+  console.log("Song finished loading, ready to play!");
+}
