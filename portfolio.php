170999054cae32aaa6bc41b6d724309241e61ee1
ErikHenriksson <ehenri@kth.segit config --global user.name ehenri>
2019-06-26 10:21:38 +0200

    Adding movieminded to portfolio

--- portfolio.php
+++ portfolio.php
@@ -1,32 +1,41 @@
 <?php
-	include_once 'header.php';
+include_once 'header.php';
 ?>
 
 <section class="main-container">
 	<section class="main-wrapper">
 		<div class="main-wrapperportfolio">
-				<h2 style="color:#FFFFFF; font-size: 50px">Games</h2>
+			<h2 style="color:#FFFFFF; font-size: 50px">Games</h2>
 
 			<div class="row">
 				<div class="column">
-				    <a href="games/pong/index.html" style="border:5px solid #ccc;">
-					<img src="images/pong.png" alt="1stGame" style="width:95%; height:95%; ">
+					<a href="games/pong/index.html" style="border:5px solid #ccc;">
+						<img src="images/pong.png" alt="1stGame" style="width:95%; height:95%; ">
 
 					</a>
 				</div>
 				<div class="column">
 					<a href="games/tictactoe/difficulty.html" style="border:5px solid #ccc;">
-				    <img src="images/TicTacToe.png" alt="2ndGame" style="width:80%; height:85%; ">
+						<img src="images/TicTacToe.png" alt="2ndGame" style="width:80%; height:85%; ">
 					</a>
 				</div>
+			</div>
+			<div class="row">
 				<div class="column">
 					<a href="games/snake/index.php" style="border:5px solid #ccc;">
-				    <img src="images/snake.jpg" alt="3rdGame" style="width:75%; height:75%; ">
+						<img src="images/snake.jpg" alt="3rdGame" style="width:75%; height:75%; ">
 					</a>
 				</div>
 				<div class="column">
 					<a href="games/maze/index.php" style="border:5px solid #ccc;">
-				    <img src="images/maze.jpg" alt="4thGame" style="width:70%; height:100%; ">
+						<img src="images/maze.jpg" alt="4thGame" style="width:70%; height:100%; ">
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="column">
+					<a href="https://movieminded.netlify.com/" style="border:5px solid #ccc;">
+						<img src="images/movieminded.png" alt="5thGame" style="width:75%; height:75%; ">
 					</a>
 				</div>
 			</div>
@@ -41,23 +50,25 @@
 
 		<div class="row">
 			<div class="column">
-			    <a href="mountain.php" style="border:5px solid #ccc;">
-				<img src="images/mountain.png" alt="1stAnimation" style="width:70%; height:65%; ">
+				<a href="mountain.php" style="border:5px solid #ccc;">
+					<img src="images/mountain.png" alt="1stAnimation" style="width:70%; height:65%; ">
 				</a>
 			</div>
 			<div class="column">
 				<a href="stars.php" style="border:5px solid #ccc;">
-			    <img src="images/stars.png" alt="2ndAnimation" style="width:70%; height:70%; ">
+					<img src="images/stars.png" alt="2ndAnimation" style="width:70%; height:70%; ">
 				</a>
 			</div>
+		</div>
+		<div class="row">
 			<div class="column">
 				<a href="transformingCube.php" style="border:5px solid #ccc;">
-			    <img src="images/transformingCube.png" alt="3rdAnimation" style="width:90%; height:90%; ">
+					<img src="images/transformingCube.png" alt="3rdAnimation" style="width:90%; height:90%; ">
 				</a>
 			</div>
 			<div class="column">
 				<a href="issLocation.php" style="border:5px solid #ccc;">
-			    <img src="images/ISS.png" alt="4thAnimation" style="width:95%; height:95%; ">
+					<img src="images/ISS.png" alt="4thAnimation" style="width:95%; height:95%; ">
 				</a>
 			</div>
 		</div>
@@ -71,23 +82,25 @@
 
 		<div class="row">
 			<div class="column">
-			    <a href="lava.php" style="border:5px solid #ccc;">
-				<img src="images/lavaFlow.png" alt="1stSound" style="width:80%; height:85%; ">
+				<a href="lava.php" style="border:5px solid #ccc;">
+					<img src="images/lavaFlow.png" alt="1stSound" style="width:80%; height:85%; ">
 				</a>
 			</div>
 			<div class="column">
 				<a href="cave.php" style="border:5px solid #ccc;">
-			    <img src="images/caveLife.png" alt="2ndSound" style="width:85%; height:85%; ">
+					<img src="images/caveLife.png" alt="2ndSound" style="width:85%; height:85%; ">
 				</a>
 			</div>
+		</div>
+		<div class="row">
 			<div class="column">
 				<a href="rain.php" style="border:5px solid #ccc;">
-			    <img src="images/rain.png" alt="3rdSound" style="width:100%; height:100%; ">
+					<img src="images/rain.png" alt="3rdSound" style="width:100%; height:100%; ">
 				</a>
 			</div>
 			<div class="column">
 				<a href="beach.php" style="border:5px solid #ccc;">
-			    <img src="images/beach.png" alt="4thSound" style="width:95%; height:95%; ">
+					<img src="images/beach.png" alt="4thSound" style="width:95%; height:95%; ">
 				</a>
 			</div>
 		</div>
@@ -101,16 +114,36 @@
 
 		<div class="row">
 			<div class="column">
+<<<<<<< HEAD
+				<a href="musicDJ.php" style="border:5px solid #ccc;">
+					<img src="images/musicDJ.png" alt="1stSound" style="width:50%; height:50%; ">
+=======
 			    <a href="musicDJ.php" style="border:5px solid #ccc;">
 				<img src="images/musicDJ.png" alt="1st" style="width:50%; height:50%; ">
+>>>>>>> 1f1a1952f063ce21bbc6182d2f710dc1dc302fab
 				</a>
 			</div>
 			<div class="column">
 				<a href="videoChris.php" style="border:5px solid #ccc;">
+<<<<<<< HEAD
+					<img src="images/Chris.png" alt="2ndSound" style="width:105%; height:105%; ">
+=======
 			    <img src="images/Chris.png" alt="2nd" style="width:105%; height:105%; ">
+>>>>>>> 1f1a1952f063ce21bbc6182d2f710dc1dc302fab
+				</a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="column">
+<<<<<<< HEAD
+				<a href="" style="border:5px solid #ccc;">
+					<img src="images/website.jpg" alt="3rdSound" style="width:100%; height:100%; ">
 				</a>
 			</div>
 			<div class="column">
+				<a href="" style="border:5px solid #ccc;">
+					<img src="images/website.jpg" alt="4thSound" style="width:100%; height:100%; ">
+=======
 				<a href="geolocation.html" style="border:5px solid #ccc;">
 			    <img src="images/searchPos.png" alt="3rd" style="width:100%; height:100%; ">
 				</a>
@@ -118,6 +151,7 @@
 			<div class="column">
 				<a href="leagueSearch.php" style="border:5px solid #ccc;">
 			    <img src="images/leagueSearch.png" alt="4th" style="width:100%; height:100%; ">
+>>>>>>> 1f1a1952f063ce21bbc6182d2f710dc1dc302fab
 				</a>
 			</div>
 		</div>
@@ -127,5 +161,5 @@
 
 
 <?php
-	include_once 'footer.php';
+include_once 'footer.php';
 ?>
